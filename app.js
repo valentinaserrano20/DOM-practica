ec3464b74fb96fc9b13712a1df3939e9b01645db
Valentina Serrano <Valentinaserrano120@gmail.com>
2026-04-22 17:35:03 -0500

    Se garego la funcionalidad siguiendo los parametros de la guia

---
+++ app.js
@@ -0,0 +1,16 @@
+const boton= document.querySelector("button");
+const mensaje= document.querySelector("#msg");
+const mostrarMensaje = document.querySelector(".mensaje");
+
+
+
+boton.addEventListener("click", (e) =>{
+    e.preventDefault()
+    const msg = mensaje.value.trim();
+    if(msg){
+        const nuevoMensaje = document.createElement("p");
+        nuevoMensaje.textContent = msg;
+        mostrarMensaje.append(nuevoMensaje)
+    }
+    mensaje.value = "";
+})
